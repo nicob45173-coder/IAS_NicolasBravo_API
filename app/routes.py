40a4e39abeb473a1796660c380df3c632b11400a
Nicolas Bravo <nicob45173@gmail.com>
2026-06-10 20:40:18 -0300

    Prueba modificando exitStatus

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ def home():
 # Healthcheck
 @main.route("/health", methods=["GET"])
 def health():
-    return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "Estoy ok"}), 200
 
 
 #  READ ALL
